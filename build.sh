a434540a6fc5e680e4bdc1ff2e91751c517e1b26
SukkaW <isukkaw@gmail.com>
2018-12-29 11:46:09 +0800

    ci: remove cache & use npm instead of yarn

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 
 git clone https://github.com/Dreamacro/clash-dashboard.git --depth=2
 cd clash-dashboard
-rm -rf .git
-cd ..
-mkdir -p workdir
-mv -f clash-dashboard/* workdir
-cd workdir
-yarn
-yarn build
+npm i
+npm run build
